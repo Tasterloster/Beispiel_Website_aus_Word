5b8fb93a67a54747408c37ab77e833e3d0eb4516
lgeldmacher <l.geldmacher@x-cell.com>
2025-10-06 17:40:36 +0200

    Starting to build

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheNavigation from '@/components/TheNavigation.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<TheNavigation></TheNavigation>
 </template>
 
 <style scoped></style>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="nav">
+    <a href="#">
+
+    </a>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
