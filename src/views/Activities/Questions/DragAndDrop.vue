55b95fb0ee7733a8e0b100e87261a0d5930365a8
lgeldmacher <l.geldmacher@x-cell.com>
2025-10-15 15:30:38 +0200

    Drag and Drop works, but only one way. Trying a different library to sort it out.

--- src/views/Activities/Questions/DragAndDrop.vue
+++ src/views/Activities/Questions/DragAndDrop.vue
@@ -3,21 +3,17 @@ import sourceData from '@/QuizItems.json'
 import {dragAndDrop, useDragAndDrop} from '@formkit/drag-and-drop/vue'
 const picturePath = '/images/quiz_images/quiz_2/'
 
-type item = {
-  name: string;
-  id: number;
-  imgName: string;
-  type: string;
-}
 
-const items: item[] = sourceData.items
-const tools: item[] =[]
-const toys: item[] =[]
+const [itemPool, items] =
+  useDragAndDrop(sourceData.items, {group: "dndQuiz"})
+const [itemDropZoneOne, tools] =
+  useDragAndDrop([], {group: "dndQuiz"})
+const [itemDropZoneTwo, toys] =
+  useDragAndDrop([], {group: "dndQuiz"})
 
-
-const [itemPool, ] = useDragAndDrop(items, {group: "dndQuiz"})
-
-console.log(items)
+console.log('items: ', items.value)
+console.log('tools: ', tools.value)
+console.log('toys: ', toys.value)
 
 
 </script>
@@ -31,7 +27,6 @@ console.log(items)
       <div class="itemAreaContainer">
         <div class="itemHeader">Itempool</div>
         <div ref="itemPool" class="itemPool">
-          <div class="itemContainer">
             <img
               class="dndImg item"
               v-for="img in items"
@@ -39,17 +34,37 @@ console.log(items)
               :src="`${picturePath}${img.imgName}.jpg`"
               :alt="img.name"
             />
-          </div>
         </div>
       </div>
       <div class="dropOffContainer">
         <div class="dropOffHeader">Gruppen</div>
         <div class="dropOffBody">
-          <div class="dropOffZone answerOne tools">
-            <p>Echte Werkzeuge</p>
+          <div
+            ref="itemDropZoneOne"
+            class="dropOffZone answerOne tools">
+            <p>Werkzeuge</p>
+<!--            <template v-for="tool in tools">-->
+<!--              {{tool}}-->
+<!--            </template>-->
+            <img
+              class="dndImg item"
+              v-for="tool in tools"
+              :key="tool.id"
+              :src="`${picturePath}${tool.imgName}.jpg`"
+              :alt="tool.name"
+            >
           </div>
-          <div class="dropOffZone answerTwo toys">
+          <div
+            ref="itemDropZoneTwo"
+            class="dropOffZone answerTwo toys">
             <p>Spielzeuge</p>
+            <img
+              class="dndImg item"
+              v-for="toy in toys"
+              :key="toy.id"
+              :src="`${picturePath}${toy.imgName}.jpg`"
+              :alt="toy.name"
+              >
           </div>
         </div>
       </div>
@@ -69,7 +84,7 @@ console.log(items)
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  gap: .3em;
+  gap: 1.5em;
   box-sizing: border-box;
   &:first-child {
     flex: 2 1 0;
@@ -98,23 +113,18 @@ console.log(items)
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
-  gap: 2em;
+  gap: .3em;
   margin-top: 1em;
-}
-.itemContainer {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  gap: 0.3em;
-}
-.item {
+  padding: 1em;
+  background-color: white;
+  width: 100%;
 }
 
 .dndImg {
   width: 200px;
   height: 200px;
-  max-width: 50%;
-  max-height: 50%;
+  max-width: 75%;
+  max-height: 75%;
   object-fit: contain;
   background-color: white;
   border: 1px solid var(--vt-c-indigo);
